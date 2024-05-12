4038bc435292909803f36239cb7eb765bc25e865
stasrusnak <stas_rusnak@gmail.com>
2024-05-12 14:38:54 +0300

    add minimap button

--- TbcChatLootIcons.lua
+++ TbcChatLootIcons.lua
@@ -2,25 +2,23 @@ local ICON_SIZE = 40
 
 
 local frame = CreateFrame("Frame")
-frame:RegisterEvent("ADDON_LOADED")  
+frame:RegisterEvent("ADDON_LOADED") 
+
 function frame:OnEvent(event, addonName)
     if event == "ADDON_LOADED" and addonName == "TbcChatLootIcons" then
         -- Загрузка сохраненных настроек
         if TbcChatLootIconsSavedVariables then
             if TbcChatLootIconsSavedVariables.ICON_SIZE then
                 ICON_SIZE = TbcChatLootIconsSavedVariables.ICON_SIZE 
-            end
-        else
-            print("Файл сохраненных настроек не найден или поврежден.")
+            end 
         end 
-end 
+    end
+end
+
 frame:SetScript("OnEvent", frame.OnEvent)
 
 -- Прочие части вашего кода
 -- ...
-
-
-
 
 
 local function SimpleRound(val, valStep)
@@ -41,8 +39,9 @@ local function CreateIconSizeSlider(parent, name, title, minVal, maxVal, valStep
     slider.textHigh:SetText(floor(maxVal))
     -- slider.textLow:SetTextColor(0.4, 0.4, 0.4)
     -- slider.textHigh:SetTextColor(0.4, 0.4, 0.4)
-    
-    editbox:SetSize(50, 30)
+    editbox:SetWidth(70)
+    editbox:SetHeight(30)
+    -- editbox:SetSize(50, 30)
     editbox:ClearAllPoints()
     editbox:SetPoint("LEFT", slider, "RIGHT", 15, 0)
     editbox:SetText(slider:GetValue())
@@ -91,10 +90,12 @@ end
 
 
  
+ 
 
 -- Фрейм для настроек аддона
-local OptionsFrame = CreateFrame("Frame", "TbcChatLootIconsOptionsFrame", UIParent, "UIPanelDialogTemplate")
-OptionsFrame:SetSize(300, 200)
+local OptionsFrame = CreateFrame("Frame", "TbcChatLootIconsOptionsFrame", UIParent )
+OptionsFrame:SetWidth(300)
+OptionsFrame:SetHeight(200) 
 OptionsFrame:SetPoint("CENTER")
 OptionsFrame.title = OptionsFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
 OptionsFrame.title:SetPoint("TOP", OptionsFrame, "TOP", 0, -5)
@@ -104,6 +105,24 @@ OptionsFrame:EnableMouse(true)
 OptionsFrame:RegisterForDrag("LeftButton")
 OptionsFrame:SetScript("OnDragStart", OptionsFrame.StartMoving)
 OptionsFrame:SetScript("OnDragStop", OptionsFrame.StopMovingOrSizing)
+ 
+OptionsFrame:SetBackdrop({
+    bgFile = "Interface\\BUTTONS\\WHITE8X8", -- Используем стандартную белую текстуру
+    edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border", -- Файл текстуры для рамки
+    edgeSize = 16, -- Размер рамки
+    insets = { left = 5, right = 5, top = 5, bottom = 5 } -- Отступы внутри рамки
+})
+OptionsFrame:SetBackdropColor(0, 0, 0, 1) -- Цвет фона (черный)
+OptionsFrame:SetBackdropBorderColor(1, 1, 1, 1) -- Цвет рамки (белый)
+
+
+-- Добавление крестика закрытия окна
+OptionsFrame.closeButton = CreateFrame("Button", nil, OptionsFrame, "UIPanelCloseButton")
+OptionsFrame.closeButton:SetPoint("TOPRIGHT", OptionsFrame, "TOPRIGHT", -10, -10)
+OptionsFrame.closeButton:SetScript("OnClick", function()
+    OptionsFrame:Hide()
+end)
+
 OptionsFrame:Hide()
 
 -- Элемент управления для выбора размера иконки
@@ -114,7 +133,8 @@ Slider:SetPoint("TOPLEFT", OptionsFrame, "TOPLEFT", 20, -50)
 -- Кнопка "Сохранить настройки"
 local SaveButton = CreateFrame("Button", "TbcChatLootIconsSaveButton", OptionsFrame, "UIPanelButtonTemplate")
 SaveButton:SetText("Сохранить настройки")
-SaveButton:SetSize(120, 25)
+SaveButton:SetWidth(120)
+SaveButton:SetHeight(25) 
 SaveButton:SetPoint("BOTTOM", OptionsFrame, "BOTTOM", 0, 15)
 
 -- Функция сохранения настроек
@@ -127,8 +147,8 @@ end
 
 -- Обработчик события для кнопки "Сохранить настройки"
 SaveButton:SetScript("OnClick", function()
-    SaveSettings()
-    print("Настройки сохранены!")
+    SaveSettings() 
+    DEFAULT_CHAT_FRAME:AddMessage("Настройки сохранены!")
 end)
  
  
@@ -138,7 +158,55 @@ local function ShowOptionsFrame()
 end
 
 -- Регистрация команды для отображения меню настроек
-SLASH_MYADDON1 = "/tbclooticons"
+SLASH_MYADDON1 = "/looticons"
 SlashCmdList["MYADDON"] = ShowOptionsFrame
 
 
+
+
+-- Create minimap button
+local minibtn = CreateFrame("Button", "TbcChatLootIconsMiniMapIcon", Minimap) 
+minibtn:SetFrameLevel(8) 
+minibtn:SetMovable(true) 
+minibtn:SetWidth(25)
+minibtn:SetHeight(25)  
+minibtn:SetNormalTexture("Interface\\Icons\\INV_SummerFest_Symbol_Medium.blp") -- Установка текстуры иконки 
+minibtn:SetPoint("BOTTOMLEFT", Minimap, "BOTTOMLEFT", 0, 0)
+  
+ 
+local myIconPos = 0
+ 
+-- Control movement
+local function UpdateMapBtn()
+    local Xpoa, Ypoa = GetCursorPosition()
+    local Xmin, Ymin = Minimap:GetLeft(), Minimap:GetBottom()
+    Xpoa = Xmin - Xpoa / Minimap:GetEffectiveScale() + 70
+    Ypoa = Ypoa / Minimap:GetEffectiveScale() - Ymin - 70
+    myIconPos = math.deg(math.atan2(Ypoa, Xpoa))
+    minibtn:ClearAllPoints()
+    minibtn:SetPoint("TOPLEFT", Minimap, "TOPLEFT", 52 - (80 * cos(myIconPos)), (80 * sin(myIconPos)) - 52)
+end
+ 
+minibtn:RegisterForDrag("LeftButton")
+minibtn:SetScript("OnDragStart", function()
+    minibtn:StartMoving()
+    minibtn:SetScript("OnUpdate", UpdateMapBtn)
+end)
+ 
+minibtn:SetScript("OnDragStop", function()
+    minibtn:StopMovingOrSizing();
+    minibtn:SetScript("OnUpdate", nil)
+    UpdateMapBtn();
+end)
+ 
+-- Set position
+minibtn:ClearAllPoints();
+minibtn:SetPoint("TOPLEFT", Minimap, "TOPLEFT", 52 - (80 * cos(myIconPos)),(80 * sin(myIconPos)) - 52)
+ 
+-- Control clicks
+minibtn:SetScript("OnClick", function()
+    ShowOptionsFrame()
+end)
+
+
+ 
